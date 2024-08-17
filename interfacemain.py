b2ccfb5dd81aa32b9ec74ee9a92d518aa3d5f184
sohamkarki999 <soham.karki@education.nsw.gov.au>
2024-08-17 21:06:48 +1000

    data clleaning on documentation and code

--- interfacemain.py
+++ interfacemain.py
@@ -6,15 +6,23 @@ import matplotlib.pyplot as plt
 exit_program = False
 
 # Dataframe Setup
+file_path = 'data/all_seasons.csv'
+og_df = pd.read_csv(file_path)
+
+columns_to_delete = ['team_abbreviation', 'age', 'player_weight', 'college', 'country', 'draft_year', 'draft_round', 'draft_number', 'gp', 'pts', 'ast', 'net_rating', 'oreb_pct', 'dreb_pct', 'usg_pct', 'ts_pct', 'ast_pct', 'season']
+nba_df = og_df.drop(columns=columns_to_delete)
+
+nba_df.to_csv('data/relevantstats', index=False)
+
 nba_df = pd.read_csv('data/all_seasons.csv')
 
 nba_df = pd.read_csv('data/all_seasons.csv',
                      header=None,
-                     names=['Player', 'Team', 'Rebounds', 'Points'])
+                     names=['Player', 'Height', 'Rebounds'])
 
 # Functions for Program
 def displayOriginalData():
-    print(nba_df)
+    print(og_df)
 
 def visualizePlayerRebounds():
     nba_df.plot(
@@ -25,3 +33,5 @@ def visualizePlayerRebounds():
                 alpha=0.3,
                 title='Player Rebounds')
     plt.show()
+
+
